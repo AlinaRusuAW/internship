b0978c9248f77874f2f09496d7dec93f2d9f00c1
AlinaRusuAW <74514427+AlinaRusuAW@users.noreply.github.com>
2022-12-11 20:42:34 +0200

    Add files via upload

--- internship/src/operators/Calculator.java
+++ internship/src/operators/Calculator.java
@@ -1,19 +1,43 @@
 package operators;
 
 import java.util.Scanner;
+import static java.lang.Math.pow;
 
 public class Calculator {
     public static void main(String[] args){
-        Scanner x = new Scanner (System.in);
-        System.out.println("Introduceti numarul a: ");
-        Integer a = x.nextInt ();
-        Scanner y = new Scanner (System.in);
-        System.out.println("Introduceti numarul b: ");
-        Integer b = y.nextInt ();
-        System.out.println (a+b);
-        System.out.println (a-b);
-        System.out.println (a*b);
-        System.out.println (a/b);
-        System.out.println (a^b);
-    }
+                char operator;
+                Double a, b, c;
+                Scanner input = new Scanner(System.in);
+                System.out.println("Alegeti operatorul: +, -, *, /, ^ ");
+                operator = input.next().charAt(0);
+                System.out.println("Introduceti numarul a: ");
+                a = input.nextDouble ();
+                System.out.println("Introduceti numarul b: ");
+                b = input.nextDouble ();
+                switch (operator) {
+                    case '+':
+                        c = a + b;
+                        System.out.println(a + " + " + b + " = " + c);
+                        break;
+                    case '-':
+                        c = a - b;
+                        System.out.println(a + " - " + b + " = " + c);
+                        break;
+                    case '*':
+                        c = a * b;
+                        System.out.println(a + " * " + b + " = " + c);
+                        break;
+                    case '/':
+                        c = a / b;
+                        System.out.println(a + " / " + b + " = " + c);
+                        break;
+                    case '^':
+                        c = pow (a,b);
+                        System.out.println(a + " ^ " + b + " = " + c);
+                        break;
+                    default:
+                        System.out.println("Operator nevalid!");
+                        break;
+                }
+            }
 }
